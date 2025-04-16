e6d77abb92a22e13300baeb14abb7c96d4727d1a
Samarth Patel <samarthnimeshkumarpatel@gmail.com>
2025-04-16 21:02:26 +0530

    modified:   subjects/aiml/p5.js

--- subjects/aiml/p5.js
+++ subjects/aiml/p5.js
@@ -5,99 +5,62 @@ const router = express.Router();
 // GET /os/p1
 router.get("/", (req, res) => {
   const codeString = `
-# %% [markdown]
-# Name: Krish Parmar
-# 
-# Roll No: 24BEE034
-# 
-# Practical No: 6
-# 
-# Aim: Write a program for classifing iris images using KNN Classifier. Implement Accuracy.
-
 # %%
 import pandas as pd
 import numpy as np
 import math
-from sklearn.model_selection import train_test_split
-# Importing the dataset values
-data = pd.read_csv("iris.csv", header = "infer").values
+data = pd.read_csv("BostonHousing.csv", header = "infer").values
 print(data)
-# Separating the input data and output data
-x = data[:, 1:-1]
+
+
+# %% [markdown]
+# Separating the Input/Output sets and Inputing the Test Split
+
+# %%
+x = data[:, 0:-1]
 y = data[:, -1]
-# Splitting the dataset into a 0.8:0.2 ratio for train:test
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, stratify=y)
-
-# %%
-# Inputting k
-k = int(input("Enter the value of k: "))
-
-# %%
-data.shape
+nrows = data.shape[0] # Number of rows in the dataset
+test_split = float(input("Enter the split between 0 and 1: "))
+print("x:", x)
+print("y:", y)
+print("Number of rows:", nrows)
+print("Test Split:", test_split)
 
 # %% [markdown]
-# KNN Classification without SKLearn
+# Splitting the Data without Scikit-learn
 
 # %%
-# Number of classes (species here)
-nclasses = np.unique(y_train).shape[0]
-# Array to store the distances
-dist = np.zeros(shape=x_train.shape[0])
-# Array to store the predictions
-pred = np.zeros(shape=x_test.shape[0])
-# Array to store the weightages
-classvotes = np.zeros(shape=nclasses)
-# Calculating for each test case
-for i in range(x_test.shape[0]):
-
- # Calculating the Euclidian Distance
- dist = np.sqrt(np.sum((x_train - x_test[i])**2, axis=1))
-
- # Storing the indices of the K-Nearest-Neighbours
- kminind = np.argpartition(dist, k)[0:k]
-
- # Inverting the distance
- invdist = 1/(dist + 10e-20)
- # For normalising
- denom = sum(invdist[kminind])
-
- # Calculating the weightages
-for j in range(k):
- classvotes[int(y_train[kminind[j]])] += invdist[kminind[j]]
-
- # Normalising
- classvotes /= denom
-
- # Making the prediction based on the class with highest weightage
- pred[i] = np.argmax(classvotes)
-# Displaying the predictions
-print(pred)
-# Calculating and displaying the accuracy
-accuracy = np.sum(pred.astype(int)==y_test.astype(int))/pred.shape[0]
-print(accuracy)
-
-
+nrows_train = math.floor((1-test_split)*nrows) # Number of rows in the training set
+all_indices = np.random.permutation(nrows) # Permutation of all indices of the data
+print("Number of rows in training set:", nrows_train)
+print("A random permutation of the indices:", all_indices)
+# Splitting the data into training and test sets
+x_train1 = x[all_indices[0:nrows_train], :]
+y_train1 = y[all_indices[0:nrows_train]]
+x_test1 = x[all_indices[nrows_train:], :]
+y_test1 = y[all_indices[nrows_train:]]
+print("Size of x_train:", x_train1.shape)
+print("Size of y_train:", y_train1.shape)
+print("Size of x_test:", x_test1.shape)
+print("Size of y_test:", y_test1.shape)
+# Displaying the union and intersection to check the split
+print("Union:", len(set(all_indices[0:nrows_train]).union(set(all_indices[nrows_train:]))))
+print("Intersection:", len(set(all_indices[0:nrows_train]).intersection(set(all_indices[nrows_train:]))))
 
 
 # %% [markdown]
-# KNN Classification with SKLearn
+# Splitting the Data with Scikit-learn
 
 # %%
-# Importing the SKLearn Functions
-from sklearn.metrics import accuracy_score, classification_report
-from sklearn.neighbors import KNeighborsClassifier
-# Creating an instance of the KNeighborsClassifier Class
-model = KNeighborsClassifier(n_neighbors = k, weights='distance')
-# Fitting the model to our data
-model.fit(x_train, y_train)
-# Storing the predictions from our test data
-pred = model.predict(x_test)
-# Calculating the accuracy of the model
-accuracy = accuracy_score(y_test, pred)
-# Displaying the information
-print("Predictions:", pred)
-print("Accuracy:", accuracy)
-# print("Classification Report:\n", classification_report(y_test, pred))
+# Using scikitlearn
+from sklearn.model_selection import train_test_split
+
+# Using scikitlearn
+x_train2, x_test2, y_train2, y_test2 = train_test_split(x, y, test_size=test_split)
+print("Size of x_train:", x_train2.shape)
+print("Size of y_train:", y_train2.shape)
+print("Size of x_test:", x_test2.shape)
+print("Size of y_test:", y_test2.shape)
 
 
 
